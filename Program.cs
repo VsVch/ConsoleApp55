4608a185ecf32a87de74c0dbf08ccad507e19dcf
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-10-05 05:47:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            bool check = 100 <= chislo && chislo <= 200 || chislo == 0;
+            if (!check)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
